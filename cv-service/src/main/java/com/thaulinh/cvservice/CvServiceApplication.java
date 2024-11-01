100dfdb9a85a494801bb7ce809e27730c47ccf7a
LuongNV <luong1c11@gmail.com>
2024-11-01 18:39:31 +0700

    first commit

---
+++ cv-service/src/main/java/com/thaulinh/cvservice/CvServiceApplication.java
@@ -0,0 +1,16 @@
+package com.thaulinh.cvservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CvServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvServiceApplication.class, args);
+    }
+
+}
---
+++ discovery-service/src/main/java/com/thaulinh/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,24 @@
+package com.thaulinh.discoveryservice;
+
+import com.netflix.discovery.shared.transport.jersey.TransportClientFactories;
+import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+    @Bean
+    public TransportClientFactories<?> jersey3TransportClientFactories() {
+        return new Jersey3TransportClientFactories();
+    }
+
+}
